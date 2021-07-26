2ae741dd02f244c022ce401088abc5951a581ae4
7ien <tien.nguyen9>
2021-07-26 22:39:48 +0700

    ok

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const https = require("https");
+const cryptoJs = require("crypto-js");
 const app = express();
 const axios = require("axios");
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +13,7 @@ app.get("/login", async (req, res) => {
   const payload = {
     iss: "TEST",
     api_key: "cR6fsuoz7K1SpMhJ6HD2WMqTzkBJk0a5",
-    jti: `cR6fsuoz7K1SpMhJ6HD2WMqTzkBJk0a5-${Date.now}`,
+    jti: `cR6fsuoz7K1SpMhJ6HD2WMqTzkBJk0a5-${Date.now()}`,
   };
   const token = jwt.sign(payload, "MhJ6HD2WMfsuoz7K1Sp0a5qTzkBJkcR6", {
     expiresIn: "2h",
@@ -19,12 +21,12 @@ app.get("/login", async (req, res) => {
       cty: "appotapay-api;v=1",
     },
   });
-  const signature = jwt.sign(
+  const siganture = cryptoJs.HmacSHA256(
     "partnerRefId=AB123&productCode=AC100&quantity=10",
     "MhJ6HD2WMfsuoz7K1Sp0a5qTzkBJkcR6"
   );
   console.log(token);
-  console.log(signature);
+  console.log(siganture);
   const fetchData = await axios({
     method: "post",
     url: "https://ebill.dev.appotapay.com/api/v1/service/shopcard/buy",
@@ -36,12 +38,38 @@ app.get("/login", async (req, res) => {
       partnerRefId: "AB123",
       productCode: "AC100",
       quantity: 10,
-      siganture: signature,
+      siganture: siganture,
     },
   });
   console.log(fetchData);
 
   res.status(201).json({ status: "success", fetchData });
+});
+
+app.get("/test", async (req, res) => {
+  const https = require("https");
+
+  https
+    .get(
+      "https://www.vietbank.com.vn/api/ApiBranch/getalltransagencymap",
+      (resp) => {
+        let data = "";
+
+        // A chunk of data has been received.
+        resp.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on("end", () => {
+          console.log(JSON.parse(data));
+          return res.status(200).json({ data: JSON.parse(data) });
+        });
+      }
+    )
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
 });
 
 app.post("/post/:token", async (req, res) => {
